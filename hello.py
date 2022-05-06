b2e70a1b7e1b651736fe6ad48017110c113cc148
jobobdev <tmvle22@gmail.com>
2022-05-06 13:33:22 +0900

    Yeardream algorithm done
    printing algoritm with vi eiditing

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 
 
 for i in range(1, 21):
-	if i % 5 == 0:
-		print('dream')
-	if i % 3 == 0:
+	if i % 15 == 0:
+		print('yeardream')
+	elif i % 3 == 0:
 		print('year')
+	elif i % 5 == 0:
+		print('dream')
 	else:
 		print(i)
 
